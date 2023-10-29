fed5e39e35f5e0cdbdf7ce5ffb1803e86a4930d0
dajun.ldj <dajun.ldj@alibaba-inc.com>
2023-10-29 18:31:09 +0800

    the rust programming language - ch4 understanding ownership

---
+++ rust/the-rust-programming-language/ch4/slices/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let my_string = String::from("hello world");
+    // first_word 可以接收 String 对象的切片作为参数
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // first_word 可以接收字符串字面量的切片作为参数
+    let word = first_word(&my_string_literal[..]);
+
+    // 由于字符串字面量本身就是切片，所以我们可以在这里将它传入函数，
+    // 而不需要使用额外的切片语法！
+    let word = first_word(my_string_literal);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
