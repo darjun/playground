675f082cd602e7584548e42878a481815edbdb0e
dajun.ldj <dajun.ldj@alibaba-inc.com>
2023-11-12 19:39:10 +0800

    the rust programming language - ch10 generic types, traits and lifetimes

---
+++ rust/the-rust-programming-language/ch10/conditions/src/main.rs
@@ -0,0 +1,25 @@
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Pair<T> {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmd_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The smallest member is y = {}", self.y);
+        }
+    }
+}
