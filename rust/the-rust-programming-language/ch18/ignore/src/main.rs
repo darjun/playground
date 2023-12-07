3d6f02b54d85301039d8cc6ef2ea214f003abb31
dajun.ldj <dajun.ldj@alibaba-inc.com>
2023-12-07 22:57:12 +0800

    the rust programming language - ch18 patterns and matching

---
+++ rust/the-rust-programming-language/ch18/ignore/src/main.rs
@@ -0,0 +1,62 @@
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
+}
+
+fn main() {
+    foo(3, 4);
+
+    ignore_part1();
+    ignore_part2();
+    ignore_part3();
+    ignore_part4();
+}
+
+fn ignore_part1() {
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+}
+
+fn ignore_part2() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        },
+    }
+}
+
+fn ignore_part3() {
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+    
+    let origin = Point { x: 0, y: 0, z: 0 };
+    
+    match origin {
+        Point {x, ..} => println!("x is {}", x),
+    }
+}
+
+fn ignore_part4() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        },
+    }
+}
