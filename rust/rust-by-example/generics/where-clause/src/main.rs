1718029726854da28bc004ae4e579b5156970f94
dajun.ldj <dajun.ldj@alibaba-inc.com>
2023-12-16 19:40:47 +0800

    rust by example - generics

---
+++ rust/rust-by-example/generics/where-clause/src/main.rs
@@ -0,0 +1,20 @@
+use std::fmt::Debug;
+
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+impl<T> PrintInOption for T where
+    Option<T>: Debug {
+    // We want `Option<T>: Debug` as our bound because that is what's
+    // being printed. Doing otherwise would be using the wrong bound.
+    fn print_in_option(self) {
+        println!("{:?}", Some(self));
+    }
+}
+
+fn main() {
+    let vec = vec![1, 2, 3];
+
+    vec.print_in_option();
+}
