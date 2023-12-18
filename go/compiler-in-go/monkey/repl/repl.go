bfa7629b8a1096617c092e8e4a150dfd0b702c6c
lidajun <dajun.ldj@alibaba-inc.com>
2023-12-18 09:44:34 +0800

    compiler in go - ch5 name tracing

--- go/compiler-in-go/monkey/repl/repl.go
+++ go/compiler-in-go/monkey/repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/darjun/compiler/monkey/compiler"
 	"github.com/darjun/compiler/monkey/lexer"
+	"github.com/darjun/compiler/monkey/object"
 	"github.com/darjun/compiler/monkey/parser"
 	"github.com/darjun/compiler/monkey/vm"
 )
@@ -15,6 +16,10 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -33,14 +38,14 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		comp := compiler.New()
+		comp := compiler.NewWithState(symbolTable, constants)
 		err := comp.Compile(program)
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
 			continue
 		}
 
-		machine := vm.New(comp.Bytecode())
+		machine := vm.NewWithGlobalStore(comp.Bytecode(), globals)
 		err = machine.Run()
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
