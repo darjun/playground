14525c3efd760505fb0ad235d40ed4db59e6cdc7
lidajun <dajun.ldj@alibaba-inc.com>
2025-01-22 10:13:19 +0800

    conan - conanfile py

---
+++ c++/conan/conanfile_py/conanfile.py
@@ -0,0 +1,28 @@
+import os
+
+from conan import ConanFile
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("zlib/1.2.11")
+        if self.settings.os != "Windows":
+            self.requires("base64/0.4.0")
+
+    def build_requirments(self):
+        if self.settings.os != "Windows":
+            self.tool_requires("cmake/3.22.6")
+
+
+    def layout(self):
+        # We make the assumption that if the compiler is msvc the
+        # CMake generator is multi-config
+        multi = True if self.settings.get_safe("compiler") == "msvc" else False
+        if multi:
+            self.folders.generators = os.path.join("build", "generators")
+            self.folders.build = "build"
+        else:
+            self.folders.generators = os.path.join("build", str(self.settings.build_type), "generators")
+            self.folders.build = os.path.join("build", str(self.settings.build_type))
